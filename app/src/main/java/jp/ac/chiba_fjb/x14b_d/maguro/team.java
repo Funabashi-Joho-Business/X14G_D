3d554efc0dc60d0e38791038e964ac7ea9a11fff
WIN_NT\x14g010 <x14g010@chiba-fjb.ac.jp>
2016-11-08 12:40:51 +0900

    タイマーGPSIDページの作成

--- app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/team.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/team.java
@@ -3,11 +3,11 @@ package jp.ac.chiba_fjb.x14b_d.maguro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class UID extends AppCompatActivity {
+public class team extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uid2);
+        setContentView(R.layout.activity_team);
     }
 }
