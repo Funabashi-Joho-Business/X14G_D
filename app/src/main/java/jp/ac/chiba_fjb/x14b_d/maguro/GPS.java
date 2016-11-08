178f2aa1cb5a4a6e157f89902c12bddeee488b03
WIN_NT\x14g010 <x14g010@chiba-fjb.ac.jp>
2016-11-08 11:27:55 +0900

    消し

--- app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/GPS.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/GPS.java
@@ -1,95 +1,29 @@
 package jp.ac.chiba_fjb.x14b_d.maguro;
-
 /**
  * Created by x14g010 on 2016/11/01.
  */
 
-import android.Manifest;
 import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
+import android.content.Context;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
-public class GPS extends Activity implements LocationListener {
+public class GPS extends Activity {
+    // GPS用
     private LocationManager mLocationManager;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-}
-
-    @Override
-    protected void onResume() {
-        if (mLocationManager != null) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                    || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-
-                mLocationManager.requestLocationUpdates(
-                        LocationManager.GPS_PROVIDER,
-//                LocationManager.NETWORK_PROVIDER,
-                        0,
-                        0,
-                        this);
-            }
-        }
-        super.onResume();
+        // GPS
+        mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        boolean gpsFlg = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        Log.d("GPS Enabled", gpsFlg?"OK":"NG");
     }
 
-    @Override
-    protected void onPause() {
-        if (mLocationManager != null) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-                    || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                mLocationManager.removeUpdates(this);
-            }
-        }
-        super.onPause();
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        Log.v("----------", "----------");
-        Log.v("Latitude", String.valueOf(location.getLatitude()));
-        Log.v("Longitude", String.valueOf(location.getLongitude()));
-        Log.v("Accuracy", String.valueOf(location.getAccuracy()));
-        Log.v("Altitude", String.valueOf(location.getAltitude()));
-        Log.v("Time", String.valueOf(location.getTime()));
-        Log.v("Speed", String.valueOf(location.getSpeed()));
-        Log.v("Bearing", String.valueOf(location.getBearing()));
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+    // GPSボタン
 
     }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        switch (status) {
-            case LocationProvider.AVAILABLE:
-                Log.v("Status", "AVAILABLE");
-                break;
-            case LocationProvider.OUT_OF_SERVICE:
-                Log.v("Status", "OUT_OF_SERVICE");
-                break;
-            case LocationProvider.TEMPORARILY_UNAVAILABLE:
-                Log.v("Status", "TEMPORARILY_UNAVAILABLE");
-                break;
-        }
-    }
-
-}
