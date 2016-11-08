c22d8686d2c997079bd9a2a26adac4c39013c5ef
WIN_NT\x14g010 <x14g010@chiba-fjb.ac.jp>
2016-11-08 10:47:52 +0900

    修正

--- app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/FullscreenActivity.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/FullscreenActivity.java
@@ -19,7 +19,7 @@ public class FullscreenActivity extends AppCompatActivity {
         View view = this.getWindow().getDecorView();
         view.setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LOW_PROFILE
-              //  | View.SYSTEM_UI_FLAG_FULLSCREEN
+               | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
