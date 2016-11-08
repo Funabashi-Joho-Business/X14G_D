9d7cce40ee5b06a12b341e3ba5f822b9d91bda80
WIN_NT\x14g010 <x14g010@chiba-fjb.ac.jp>
2016-11-08 11:37:22 +0900

    no message

--- app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/UID.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/UID.java
@@ -1,13 +1,46 @@
 package jp.ac.chiba_fjb.x14b_d.maguro;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.content.Context;
 
-public class UID extends AppCompatActivity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.RandomAccessFile;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_uid);
+/**
+ * Created by x14g010 on 2016/11/01.
+ */
+
+public class UID {
+    private static String sID = null;
+    private static final String getsID= "INSTALLATION";
+
+    public synchronized static String id(Context context) {
+        if (sID == null) {
+            File installation = new File(context.getFilesDir(), getsID);
+            try {
+                if (!installation.exists())
+                    writeInstallationFile(installation);
+                sID = readInstallationFile(installation);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return sID;
+    }
+
+    private static String readInstallationFile(File installation) throws IOException {
+        RandomAccessFile f = new RandomAccessFile(installation, "r");
+        byte[] bytes = new byte[(int) f.length()];
+        f.readFully(bytes);
+        f.close();
+        return new String(bytes);
+    }
+
+    private static void writeInstallationFile(File installation) throws IOException {
+        FileOutputStream out = new FileOutputStream(installation);
+        String id = UID.getsID.toString();
+        out.write(id.getBytes());
+        out.close();
     }
 }
