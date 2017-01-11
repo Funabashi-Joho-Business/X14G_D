ffb422bc6ea84237a6992158c544a09cd7b37831
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-01-11 11:35:19 +0900

    録画調整

--- app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/CameraPreview.java
+++ app/src/main/java/jp/ac/chiba_fjb/x14b_d/maguro/CameraPreview.java
@@ -342,6 +342,10 @@ public class CameraPreview implements TextureView.SurfaceTextureListener,  Camer
         mRec.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         mRec.setVideoEncoder(MediaRecorder.VideoEncoder.MPEG_4_SP);
         mRec.setVideoFrameRate(30);
+        
+        Camera.Parameters params = mCamera.getParameters();
+        mRec.setVideoSize(params.getPreviewSize().width,params.getPreviewSize().height);
+
 
         mRec.setOutputFile(fileName);
         int i;
